c33b6fb9030a062cab99dd6a265826adc2c93151
marcin03 <marcinmulawa92@gmail.com>
2016-12-18 16:05:26 +0100

    Update code.js
    Add options about Draw.

--- code.js
+++ code.js
@@ -115,4 +115,9 @@ function btnPlayAgain() {
 		document.getElementById("b9").disabled = true;	
 		anybodyWin=true;
 	}
+	else if(v1!=" "&&v2!=" "&&v3!=" "&&v4!=" "&&v5!=" "&&v6!=" "&&v7!=" "&&v8!=" "&&v9!=" "){
+		alert("Draw!");
+		document.getElementById("p1").innerHTML =("The DRAW");
+		anybodyWin=true;
+	}
 }
